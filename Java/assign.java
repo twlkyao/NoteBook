afa99a2c53bd52ba27dc323fe7affe7edb63a7b7
twlkyao <qishiyao2008@126.com>
2013-12-31 09:53:41 +0800

    Complete assign.java file.

--- Java/assign.java
+++ Java/assign.java
@@ -1,9 +1,13 @@
 /**
  * The result of a is -24,
+ * in Java they are copy 
+ * rather than reference.
  * The code can be spilt into:
- * 1. a -= a * a; // a = -30.
- * 2. a += -30; // where a = 6, so the result is -24.
- * different with C/C++, in C/C++ the result will be -60.
+ * 1. a1 -= a * a; // a1 = -30,
+ * 2. a += -a1; // where a = 6,
+ * so the result is -24.
+ * different with C/C++,
+ * in C/C++ the result will be -60.
  */
 int a = 6;
 a += a -= a * a;
